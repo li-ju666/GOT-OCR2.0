20360ef54751c0d48db87eaca095a99555c32b8f
Li Ju <li-ju666@outlook.com>
2024-11-08 14:37:59 +0100

    pdf2text script updated: file names with space can be handled (rename)

--- GOT-OCR-2.0-master/GOT/pdf2text.sh
+++ GOT-OCR-2.0-master/GOT/pdf2text.sh
@@ -9,11 +9,19 @@ mkdir -p $IMG_FOLDER
 # iterate over all the pdf files
 for pdf_file in $PDF_FOLDER/*.pdf
 do
+    # # remove all the spaces in the pdf file name
+    # # there might be multiple spaces in the file name
+    # if [[ $pdf_file == *" "* ]]; then
+    #     new_pdf_file=$(echo $pdf_file | tr -s ' ')
+    #     mv "$pdf_file" $new_pdf_file
+    #     pdf_file=$new_pdf_file
+    # fi
+
     # image folder name
     image_folder=$IMG_FOLDER/$(basename $pdf_file .pdf)
     txt_folder=$TXT_FOLDER/$(basename $pdf_file .pdf)
 
-    echo "Processing $pdf_file file..."
+    echo "Processing $pdf_file ..."
 
     # convert pdf to images
     python3 pdf2images.py $pdf_file $image_folder
@@ -25,3 +33,4 @@ do
     rm -rf $image_folder
 
 done
+echo "Done!"
